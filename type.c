d964f703fadc66d239f16cf22c97e3e85bec6086
mizunoyuuki <mizuno@careerindex.co.jp>
2026-03-26 14:09:57 +0000

    type.cで型の情報を伝播させるように追加中

--- type.c
+++ type.c
@@ -7,6 +7,36 @@ void add_type(Node *node){
     if (node == NULL) return;
     if (node->type != NULL) return;
 
+    switch (node->kind){
+          case ND_IF:
+              add_type(node->cond);
+              add_type(node->then);
+              add_type(node->els);
+              break;
+          case ND_FOR:
+              add_type(node->finit);
+              add_type(node->fcond);
+              add_type(node->finc);
+              add_type(node->fthen);
+              break;
+          case ND_BLOCK:
+              for (Node *n = node->body; n; n = n->next)
+                  add_type(n);
+              break;
+          case ND_FUNCDEF:
+              for (Node *n = node->farg_body; n; n = n->next_farg)
+                  add_type(n);
+              for (Node *n = node->func_body; n; n = n->next_func_stmt)
+                  add_type(n);
+              break;
+          case ND_FUNCALL:
+              for (Node *n = node->farg_body; n; n = n->next_farg)
+                  add_type(n);
+              break;
+          default:
+              break;
+    }
+
     add_type(node->lhs);
     add_type(node->rhs);
 
@@ -14,6 +44,53 @@ void add_type(Node *node){
         case ND_ADD:
             // &a + 1; とか片方がND_NUMの時は、コンパイル時に&aがintのポインタ型であれば1*4とかに変換できる。
             // TODO: 一旦シンプルに。charとか増やした時単なるINT or PTRだけの処理ではだめ。
+            if (node->lhs->type->kind == TY_INT &&  node->rhs->type->kind == TY_INT){
+                Type *type = calloc(1, sizeof(Type));
+                type->kind = TY_INT;
+                type->size = INT_SIZE;
+                node->type = type;
+
+                return;
+            }
+
+            if (node->lhs->type->kind == TY_PTR && node->rhs->type->kind == TY_INT){
+                // 型を受け継ぐ
+                Type *head = calloc(1, sizeof(Type));
+                Type *cur = head;
+                cur->kind = TY_PTR;
+                cur->to_ptr = calloc(1, sizeof(Type));
+                cur = cur->to_ptr;
+                cur->size = INT_SIZE;
+                cur->kind = TY_INT;
+                node->type = head;
+
+                // &x + 1 といったポインタ変数　+ NUMの計算になる。
+                Node *tmp = node->rhs;
+                node->rhs = new_node(ND_MUL, tmp, new_node_num(INT_SIZE));
+
+                return;
+            }
+
+            if (node->lhs->type->kind == TY_INT && node->rhs->type->kind == TY_PTR){
+                // 型を受け継ぐ
+                Type *head = calloc(1, sizeof(Type));
+                Type *cur = head;
+                cur->kind = TY_PTR;
+                cur->to_ptr = calloc(1, sizeof(Type));
+                cur = cur->to_ptr;
+                cur->size = INT_SIZE;
+                cur->kind = TY_INT;
+                node->type = head;
+
+                // &x + 1 といったポインタ変数　+ NUMの計算になる。
+                Node *tmp = node->lhs;
+                node->lhs = new_node(ND_MUL, tmp, new_node_num(INT_SIZE));
+
+                return;
+            }
+            break;
+
+        case ND_SUB:
             if (node->lhs->type->kind == TY_INT &&  node->rhs->type->kind == TY_INT){
                 Type *type = calloc(1, sizeof(Type));
                 type->kind = TY_INT;
@@ -37,40 +114,37 @@ void add_type(Node *node){
                 node->rhs = new_node(ND_MUL, tmp, new_node_num(INT_SIZE));
             }
 
-            if (node->lhs->type->kind == TY_INT && node->rhs->type->kind == TY_PTR){
-                // 型を受け継ぐ
-                Type *head = calloc(1, sizeof(Type));
-                Type *cur = head;
-                cur->kind = TY_PTR;
-                cur->to_ptr = calloc(1, sizeof(Type));
-                cur = cur->to_ptr;
-                cur->size = INT_SIZE;
-                cur->kind = TY_INT;
-                node->type = head;
+            break;
 
-                // &x + 1 といったポインタ変数　+ NUMの計算になる。
-                Node *tmp = node->lhs;
-                node->lhs = new_node(ND_MUL, tmp, new_node_num(INT_SIZE));
+        case ND_MUL:
+            if (node->lhs->type->kind == TY_INT &&  node->rhs->type->kind == TY_INT){
+                Type *type = calloc(1,sizeof(Type));
+                type->kind = TY_INT;
+                type->size = INT_SIZE;
+                node->type = type;
+            }
+            break;
+        case ND_DIV:
+            if (node->lhs->type->kind == TY_INT &&  node->rhs->type->kind == TY_INT){
+                Type *type = calloc(1,sizeof(Type));
+                type->kind = TY_INT;
+                type->size = INT_SIZE;
+                node->type = type;
             }
             break;
 
-        case ND_SUB:
-            break;
-        case ND_MUL:
-            break;
-        case ND_DIV:
-            break;
         case ND_EQ:
-            break;
         case ND_NE:
-            break;
         case ND_LT:
-            break;
         case ND_LE:
-            break;
         case ND_GT:
-            break;
         case ND_GE:
+            Type *type = calloc(1,sizeof(Type));
+            type->kind = TY_INT;
+            type->size = INT_SIZE;
+
+            node->type = type;
+
             break;
         case ND_ASSIGN:
             break;
@@ -78,17 +152,7 @@ void add_type(Node *node){
             break;
         case ND_RETURN:
             break;
-        case ND_IF:
-            break;
         case ND_WHILE:
-            break;
-        case ND_FOR:
-            break;
-        case ND_BLOCK:
-            break;
-        case ND_FUNCALL:
-            break;
-        case ND_FUNCDEF:
             break;
         case ND_ADDR:
             // TODO:ここにもなんか実装する必要がある気がする
@@ -103,8 +167,16 @@ void add_type(Node *node){
             return;
             break;
         case ND_DEREF:
+            node->type = node->lhs->type->to_ptr;
             break;
         case ND_NUM:
+            break;
+        case ND_FUNCALL:
+            Type *t = calloc(1, sizeof(Type));
+            t->kind = TY_INT;
+            t->size = INT_SIZE;
+            node->type = t;
+        default:
             break;
     }
 
